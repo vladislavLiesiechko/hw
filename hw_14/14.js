6fb05b2489f9be6f952da56fed0c98d5c12a90aa
vladislavLiesiechko <36047217+vladislavLiesiechko@users.noreply.github.com>
2020-10-10 16:37:48 +0300

    Update 14.js

--- hw_14/14.js
+++ hw_14/14.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function Unit(type, health, maxHealth, maxDistance) {
     this.type = type;
     this.health = health;
@@ -5,23 +7,22 @@ function Unit(type, health, maxHealth, maxDistance) {
     this.maxDistance = maxDistance;
 }
 
-Unit.prototype.isReadyToMove = function () {
-    return this.maxDistance;
+Unit.prototype.isReadyToMove = function (distance) {
+    return distance <= this.maxDistance;
 }
-let unit = new Unit(10,20,100, 200);
-console.log(unit.isReadyToMove());
+
 Unit.prototype.isReadyToFight = function () {
     return this.maxHealth / 2 <= this.health;
 }
 
 
 Unit.prototype.restore = function () {
-    if (this.health <= this.maxHealth) {
-        return this.maxHealth;
-    }
+    this.health = this.health <= this.maxHealth ? this.maxHealth : this.health;
+    return this.health;
 }
-Unit.prototype.clone = function () {
-    const unit = new Unit( this.type, this.health, this.maxHealth, this.maxDistance);
+
+Unit.prototype.clone = function (unit) {
+    return new Unit(this.type, this.health, this.maxHealth, this.maxDistance);
 }
 
 function Army(defaultUnits) {
@@ -29,9 +30,39 @@ function Army(defaultUnits) {
     if (defaultUnits) this.combineUnits(defaultUnits);
 }
 
-// Army.prototype.isReadyToMove = function
-//     // Army.prototype.isReadyToFight;
-// Army.prototype.restore;
-// Army.prototype.getReadyToMoveUnits;
-// Army.prototype.combineUnits;
-// Army.prototype.cloneUnit;
+
+Army.prototype.isReadyToMove = function () {
+    return this.units.every((unit) => unit.isReadyToMove());
+}
+
+
+Army.prototype.isReadyToFight = function () {
+    return this.units.every((unit) => unit.isReadyToFight());
+}
+
+
+Army.prototype.restore = function () {
+    for (const unit of this.units) {
+        unit.restore();
+    }
+}
+Army.prototype.getReadyToMoveUnits = function () {
+    return this.units.filter((unit) => unit.isReadyToMove());
+}
+
+Army.prototype.combineUnits = function (unit) {
+    this.units = this.units.concat(unit);
+    return this.units;
+}
+
+Army.prototype.cloneUnit = function (numberOfUnits) {
+    const clonedUnit = this.units[numberOfUnits].clone();
+    return clonedUnit;
+}
+
+const firstUnit = new Unit('Spider-Man', 90, 200, 400);
+const secondUnit = new Unit('Iron Man', 200, 400, 800)
+const heroesArray = [firstUnit, secondUnit];
+const heroes = new Army(heroesArray);
+copyOfUnit = heroes.cloneUnit(1);
+heroes.combineUnits(copyOfUnit);
